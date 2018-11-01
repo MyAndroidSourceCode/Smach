4bbd5c77de97e5d7fa9bb060a3702c5fbdba6a7e
seyhamad <seyha@workwithmad.com>
2018-11-01 15:04:13 +0700

    login activity

---
+++ app/src/main/java/com/seyha/smack/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.seyha.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+    fun onLoginClicked(view:View) {
+
+    }
+
+    fun onSignUpClicked(view: View) {
+
+    }
+}
--- app/src/main/java/com/seyha/smack/MainActivity.kt
+++ app/src/main/java/com/seyha/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.seyha.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -36,4 +38,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun navHeaderLoginClicked(view: View) {
+        val loginActivityIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginActivityIntent)
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMessageClicked(view: View) {
+
+    }
 }
